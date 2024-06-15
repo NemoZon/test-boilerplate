42a9de2072a5e07251918864dcfa9cbf6cf6eccb
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-15 21:44:59 +0200

    feat: shared axios config added

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Arrow, Box, Logo } from '../shared';
 import './App.css';
+import { $api } from '../shared/api';
+import { AxiosResponse } from 'axios';
 
 const App = () => {
+  useEffect(() => {
+    const res: Promise<AxiosResponse<{ message: string }>> = $api.get('/');
+    res.then((r) => {
+      console.log(r.data.message);
+    });
+  }, []);
   return (
     <div>
       <Arrow />
