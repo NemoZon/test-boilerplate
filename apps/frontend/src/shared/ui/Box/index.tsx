d4daeef00ac70d06327df8d69b8e2595917067c6
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-15 18:24:44 +0200

    refactor: styled components taken out the Box component

--- apps/frontend/src/shared/ui/Box/index.tsx
+++ apps/frontend/src/shared/ui/Box/index.tsx
@@ -6,23 +6,24 @@ type Props = {
   backgroundColor: string;
 };
 
+const Container = styled.div`
+  display: flex;
+`;
+const Content = styled.div<{ backgroundColor: string }>`
+  background-color: ${(props) => props.backgroundColor};
+  min-width: 100px;
+  padding: 0 10px;
+  height: 100px;
+  display: flex;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Box: FC<Props> = ({ content, backgroundColor }) => {
-  const Container = styled.div`
-    display: flex;
-  `;
-  const Content = styled.div`
-    background: ${backgroundColor};
-    min-width: 100px;
-    padding: 0 10px;
-    height: 100px;
-    display: flex;
-    border-radius: 10px;
-    justify-content: center;
-    align-items: center;
-  `;
   return (
     <Container>
-      <Content>
+      <Content backgroundColor={backgroundColor}>
         <p>Action {content}</p>
       </Content>
     </Container>
