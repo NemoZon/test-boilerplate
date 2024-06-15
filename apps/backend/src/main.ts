ba5e5ca2ae1d87c7995ce4e5fb469089d2f351e2
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-15 22:06:56 +0200

    feat: env and cors config added

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
+app.use(cors(corsOptions))
+
+app.get('/api', (req, res) => {
   const test = process.env.TEST;
 
   res.send({ message: 'Hello API' });
