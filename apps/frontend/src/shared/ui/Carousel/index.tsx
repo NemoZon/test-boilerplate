8b4e7a4c1a28266fefc12e91bbf4764e228b925f
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-15 21:02:46 +0200

    feat: shared Carousel component added

---
+++ apps/frontend/src/shared/ui/Carousel/index.tsx
@@ -0,0 +1,34 @@
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import React, { FC, PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  width?: string;
+  customSettings?: Settings;
+};
+
+const Container = styled.div<{ width?: string }>`
+  width: ${(props) => props.width || '60%'};
+`;
+
+export const Carousel: FC<PropsWithChildren<Props>> = ({
+  children,
+  width,
+  customSettings,
+}) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Container width={width}>
+      <Slider {...(customSettings || settings)}>{children}</Slider>
+    </Container>
+  );
+};
