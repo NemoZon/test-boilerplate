b1278a64f45821dbc11f2020c08a4792d0369552
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-15 17:34:53 +0200

    refactor: FC type added to Arrow component

--- apps/frontend/src/shared/ui/Arrow/index.tsx
+++ apps/frontend/src/shared/ui/Arrow/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { FC } from 'react';
 import ArrowImage from './images/Arrow.png';
 
-export const Arrow = () => {
+export const Arrow: FC = () => {
   return <img src={ArrowImage} alt="arrow" />;
 };
