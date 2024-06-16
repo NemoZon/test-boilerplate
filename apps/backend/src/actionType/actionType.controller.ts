cec7fa87c6aa370351885ced028fa052b7df2e39
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-16 17:22:28 +0200

    refactor: GET /api/actiontype endpoint passed the tests

--- apps/backend/src/actionType/actionType.controller.ts
+++ apps/backend/src/actionType/actionType.controller.ts
@@ -4,7 +4,20 @@ import { actionTypeRepository } from "./actionType.repository";
 
 export class ActionTypeController {
     public static async getAll(req: Request, res: Response): Promise<void> {
-        const result = await actionTypeRepository.findAll();
-        res.status(200).json(result);
+        try {
+            const result = await actionTypeRepository.findAll();
+            if (result.length > 0) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ error: 'No action types found' });
+            }
+        } catch (error) {
+            console.error("Failed to fetch action types: ", error);
+            if (error instanceof Error) {
+                res.status(500).json({ error: "Internal server error", details: error.message });
+            } else {
+                res.status(500).json({ error: "Unknown error" });
+            }
+        }
     }
 }
