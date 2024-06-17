e79bc596540daf74f792cddb4ae456c0dd0e1d67
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-17 21:58:32 +0200

    code: Queue.start called

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import cors, { CorsOptions } from 'cors';
 import { ActionTypeRouter } from './actionType/';
 import { ActionRouter } from './action/action.router';
+import { Queue } from './services/queue';
 
 export const app: Express = express();
 
@@ -15,3 +16,5 @@ app.use(cors(corsOptions))
 
 app.use('/api/actiontype', ActionTypeRouter)
 app.use('/api/action', ActionRouter)
+
+Queue.start(15000)
