8e0974cdded42cef173d05af5d19f0d3e2ae1dbc
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-17 16:01:04 +0200

    test: GET /api/action/:id endpoint

--- apps/backend/tests/action/index.test.ts
+++ apps/backend/tests/action/index.test.ts
@@ -53,5 +53,58 @@ describe('Test /api/action', () => {
             expect(response.body).toEqual([]);
         });
     })
+    describe('GET /api/action/:id', () => {
+        test('return 400 if the id is not a valid ObjectId', async () => {
+            const response = await request(app).get('/api/action/hello');
 
+            expect(response.statusCode).toBe(400);
+            expect(response.body.error).toBe('Invalid id');
+        });
+        test('return 404 when no action', async () => {
+            await actionRepository.clear()
+            const response = await request(app).get('/api/action/666ee56810e0fce8e846effa');
+            expect(response.statusCode).toBe(404);
+            expect(response.body.error).toBe('No action found');
+        });
+        test('return 200 when the action is found', async () => {
+            await actionRepository.populate(1, createActionData)
+            const actionResponse = await request(app).get('/api/action/');
+            const id = actionResponse.body[0]._id
+            const response = await request(app).get(`/api/action/${id}`);
+
+            expect(response.statusCode).toBe(200);
+
+            expect(response.body).toHaveProperty('ActionType');
+            expect(typeof response.body.ActionType).toBe('string');
+
+            expect(response.body).toHaveProperty('createdAt');
+            expect(new Date(response.body.createdAt).getTime()).not.toBeNaN();
+
+            expect(Object.values(ActionStatusEnum)).toContain(response.body.status);
+        });
+        test('check Action values', async () => {
+            await actionRepository.clear();
+            await actionTypeRepository.clear();
+
+            await actionTypeRepository.insert(createActionTypeData());
+            const actionTypeResponse = await request(app).get('/api/actiontype');
+            const actionTypeId = actionTypeResponse.body[0]._id
+
+            const now = new Date;
+            await actionRepository.insert(createActionData({
+                ActionType: actionTypeId,
+                createdAt: now,
+                status: ActionStatusEnum.WAITING
+            }))
+
+            const actionsResponse = await request(app).get('/api/action');
+            const id = actionsResponse.body[0]._id
+
+            const response = await request(app).get(`/api/action/${id}`);
+
+            expect(response.body.ActionType).toBe(actionTypeId);
+            expect(new Date(response.body.createdAt)).toEqual(now);
+            expect(response.body.status).toBe(ActionStatusEnum.WAITING);
+        });
+    })
 })
