23ddc9f0390cb80c448c9e55056225d0df139354
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-17 16:06:33 +0200

    code: GET /api/action/:id endpoint added

--- apps/backend/src/action/action.controller.ts
+++ apps/backend/src/action/action.controller.ts
@@ -15,4 +15,28 @@ export class ActionController {
             }
         }
     }
+
+    public static async getById(req: Request, res: Response): Promise<void> {
+        try {            
+            const id = req.params.id
+            const objectIdRegex = /[0-9a-f]{24}/
+            if (!objectIdRegex.test(id)) {
+                res.status(400).json({ error: 'Invalid id' });
+                return;
+            }
+            const result = await actionRepository.findById(id);
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ error: 'No action found' });
+            }
+        } catch (error) {
+            console.error("Failed to fetch action types: ", error);
+            if (error instanceof Error) {
+                res.status(500).json({ error: "Internal server error", details: error.message });
+            } else {
+                res.status(500).json({ error: "Unknown error" });
+            }
+        }
+    }
 }
--- apps/backend/src/action/action.router.ts
+++ apps/backend/src/action/action.router.ts
@@ -4,3 +4,4 @@ import { ActionController } from "./action.controller";
 export const ActionRouter = Router();
 
 ActionRouter.get('/', ActionController.getAll);
+ActionRouter.get('/:id', ActionController.getById);
