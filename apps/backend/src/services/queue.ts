d3fb04b2e93a8c9b27db297f1f84f29763acb60c
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-18 21:26:23 +0200

    fix: interval moved from wss

--- apps/backend/src/services/queue.ts
+++ apps/backend/src/services/queue.ts
@@ -2,10 +2,12 @@ import { ActionData, actionRepository } from "../action";
 import http from 'http';
 import WebSocket from 'ws';
 import { Express } from 'express';
+import { ObjectId } from "mongodb";
 
 export class Queue {
     private static IntervalId: NodeJS.Timer;
     private static nextAction: ActionData | null;
+    private static lastDeleted: ObjectId | undefined;
     private static delay: number;
     private static countdown: number;
     private static server: http.Server | undefined;
@@ -13,8 +15,12 @@ export class Queue {
 
     private static async deleteNextAction(): Promise<void> {
         if (this.nextAction && this.nextAction._id) {
-            await actionRepository.deleteOne(this.nextAction._id?.toString())
-            this.nextAction = null
+            const { _id, error } = await actionRepository.deleteOne(this.nextAction._id?.toString())
+            if (error) {
+                this.nextAction = null
+                return
+            }
+            this.lastDeleted = _id
         }
     }
 
@@ -39,7 +45,7 @@ export class Queue {
 
         this.IntervalId = setInterval(async () => {
             if (this.countdown > 0) {
-                this.countdown -=  1;
+                this.countdown -= 1;
             } else {
                 await this.setNextAction()
                 await this.deleteNextAction()
@@ -47,12 +53,12 @@ export class Queue {
             }
         }, 1000);
 
-        wss.on('connection', (ws) => {            
+        wss.on('connection', (ws) => {
             const sendTime = async () => {
                 if (this.countdown > 0) {
                     ws.send(JSON.stringify({ countdown: this.countdown }));
                 } else {
-                    ws.send(JSON.stringify({ countdown: this.countdown, lastDeleted: this.nextAction}));
+                    ws.send(JSON.stringify({ countdown: this.countdown, lastDeleted: this.lastDeleted?.toString() }));
                 }
             };
             ws.send(JSON.stringify({ countdown: this.countdown }));
