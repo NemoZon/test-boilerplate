72b782613a87636e9d36704927b3b5d2a2220703
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-18 14:39:18 +0200

    code: fetchAllActions error handler added

--- apps/frontend/src/entities/action/actions/index.ts
+++ apps/frontend/src/entities/action/actions/index.ts
@@ -1,14 +1,22 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { Action } from '../type'
 import { $api } from '../../../shared'
-import { AxiosResponse } from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export const fetchAllActions = createAsyncThunk(
     'actions/fetchAllActions',
     async (): Promise<Action[]> => {
-        const response: AxiosResponse<Action[]> = await $api.get("/action")
-        console.log(response.data);
-        
-        return response.data
+        try {
+            const response: AxiosResponse<Action[]> = await $api.get("/action")        
+            return response.data
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                if (error.response) {                    
+                    throw new Error(error.response.data.error);
+                }
+            }
+            throw new Error("An unexpected error occurred");
+        }
+
     }
 )
--- apps/frontend/src/shared/types/base/index.d.ts
+++ apps/frontend/src/shared/types/base/index.d.ts
@@ -1,7 +1,6 @@
 export interface BaseState {
     status: 'loading' | 'succeeded' | 'failed' | 'idle',
     error: string | null;
-    statusCode: number | null;
 }
 export interface BaseEntity {
     _id: string
