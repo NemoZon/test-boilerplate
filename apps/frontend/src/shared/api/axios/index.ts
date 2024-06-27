66e81e00e77a4e007ee24f8689ee48eb284aee0f
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-28 01:10:40 +0200

    refactor: API url changed

--- apps/frontend/src/shared/api/axios/index.ts
+++ apps/frontend/src/shared/api/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:3000/api";
+export const API_URL = "https://test-boilerplate-gqxl.onrender.com/api";
 
 export const $api = axios.create({
     baseURL: API_URL,
