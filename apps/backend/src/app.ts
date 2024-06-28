bfaac137a6b253bda2a6ac7e0b5bc756b6ff2bb8
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2024-06-28 13:24:40 +0200

    feat: proxy

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,6 +4,7 @@ import { ActionTypeRouter } from './actionType/';
 import { ActionRouter } from './action/action.router';
 import { Queue } from './services/queue';
 import { CreditRouter } from './credit';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const app: Express = express();
 
@@ -18,5 +19,11 @@ app.use(cors(corsOptions))
 app.use('/api/actiontype', ActionTypeRouter)
 app.use('/api/action', ActionRouter)
 app.use('/api/credit', CreditRouter)
+
+app.use('/ws', createProxyMiddleware({
+    target: `ws://${process.env.HOST}:${process.env.WSS_PORT}`,
+    changeOrigin: true,
+    ws: true
+  }));
 
 Queue.start(app, 15, 600)
